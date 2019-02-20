5a32859e68e8ae6c8fd9f90285333318f02aa3c9
Jaylene <l592965@163.com>
2019-02-20 11:41:57 +0800

    编写静态组件

--- react/src/components/app.jsx
+++ react/src/components/app.jsx
@@ -1,16 +1,27 @@
 import React , {Component} from 'react'   // 切记Component 的 C 必须大写
-
-import logo from '../logo.svg'
-
-import '../index.css'
-
+import ReactDom from 'react-dom'
+import Add from './add'
+import List from './list'
 export default class App extends Component {
     render() {
         return (
             <div>
-                <img className='logo' src={logo} alt="logo"/>
-                <p className='title'>这是react　组件</p>
+                <header className="site-header jumbotron">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xs-12">
+                                <h1>请发表对React的评论</h1>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <div className="container">
+                    <Add/>
+                    <List/>
+                </div>
             </div>
         )
     }
 }
+
+ReactDom.render('<App />', document.getElementById('root'))
